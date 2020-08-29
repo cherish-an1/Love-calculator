667b4bb02e9dee7675296a93a8fb73b12045f313
cherish-an1 <eanozie98@gmail.com>
2020-08-29 09:12:58 +0100

    cherish-an1

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function checkLovePercent() {
         explanation.style.visibility = "visible";
         resultNumber.style.visibility = "visible";
         resultNumber.innerHTML = loveScore + "%";
-        img.src = "giphy1.gif";
+        img.src = "asset/giphy1.gif";
         src.appendChild(img);
         explanation.innerHTML = "Perfect!!!... Love dokita is pleased to announce to you that this ship has sailed." + yourName + "" + crushName + "" + ", best love story in history. ";
         hideForResult.style.display = "none";
@@ -40,7 +40,7 @@ function checkLovePercent() {
         explanation.style.visibility = "visible";
         resultNumber.style.visibility = "visible";
         resultNumber.innerHTML = loveScore + "%";
-        img.src = "giphy2.gif";
+        img.src = "asset/giphy2.gif";
         src.appendChild(img);
         explanation.innerHTML = "Awwww.... Love dokita is happy to tell you that you've found that special one. Now is the time to make memories and enjoy every moment. ";
         hideForResult.style.display = "none";
@@ -51,7 +51,7 @@ function checkLovePercent() {
         explanation.style.visibility = "visible";
         resultNumber.style.visibility = "visible";
         resultNumber.innerHTML = loveScore + "%";
-        img.src = "giphy3.gif";
+        img.src = "asset/giphy3.gif";
         src.appendChild(img);
         explanation.innerHTML = "Hmmmm.... Love dokita isnt quite sure about this ship, but there is a very high chance of it sailing or maybe not. Anyway, still give it your best shot!!!. Follow your heart, go after what you want.";
         hideForResult.style.display = "none";
@@ -62,7 +62,7 @@ function checkLovePercent() {
         explanation.style.visibility = "visible";
         resultNumber.style.visibility = "visible";
         resultNumber.innerHTML = loveScore + "%";
-        img.src = "giphy4.gif";
+        img.src = "asset/giphy4.gif";
         src.appendChild(img);
         explanation.innerHTML = "Oopss... Someone is ganna get their heart broken again. Love dokita thinks that the chances of a relationship working out is very low. I'll advice you move on, save yourself further heart damage. ";
         hideForResult.style.display = "none";
@@ -73,7 +73,7 @@ function checkLovePercent() {
         explanation.style.visibility = "visible";
         resultNumber.style.visibility = "visible";
         resultNumber.innerHTML = loveScore + "%";
-        img.src = "giphy5.gif";
+        img.src = "asset/giphy5.gif";
         src.appendChild(img);
         explanation.innerHTML = "Lol... This ship is unsailable, Love dokita insists that you give up on love. I'll advice you focus on yourself. Get a hobby!!!. " ;
         hideForResult.style.display = "none";
